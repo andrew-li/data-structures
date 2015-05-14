b3de97ca2a5820da79c613b9130bd16833daf24f
andrew-li <ali91007@gmail.com>
2015-05-14 10:19:11 -0700

    LL complete

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,37 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    if(list.tail === null){
+      list.head = new Node(value);
+      list.tail = list.head;
+    }
+    else{
+      list.tail.next = new Node(value);
+      list.tail = list.tail.next;
+    }
   };
 
   list.removeHead = function(){
+    if(list.head === null){
+      return null;
+    }
+    else{
+      var temp = list.head.value;
+      list.head = list.head.next;
+      return temp;
+    }
   };
 
   list.contains = function(target){
+    var tempNode = list.head;
+    while(tempNode){
+      if(tempNode.value === target){
+        return true;
+      }
+      tempNode = tempNode.next;
+    }
+    return false;
+
   };
 
   return list;
