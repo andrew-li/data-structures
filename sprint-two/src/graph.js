1f9e967eb2cfbacbb493d7a7bdeccaa466420260
andrew-li <ali91007@gmail.com>
2015-05-14 16:05:59 -0700

    finished tree and started graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,18 +1,128 @@
 
 
 var Graph = function(){
+  this.clusters = {};
+  this.clusterCount = 0;
+  this.totalNodeCount = 0;
 };
 
-Graph.prototype.addNode = function(node){
+var gNode = function(id)
+{
+  this.id = id;
+  this.value;
+  this.adjList = {};
+  this.adjCount = 0;
 };
 
-Graph.prototype.contains = function(node){
+var Cluster = function(id)
+{
+  this.id = id;
+  this.nodes = {};
+  this.nodeCount = 0;
 };
 
-Graph.prototype.removeNode = function(node){
+Graph.prototype.addNode = function(nodeValue){
+  var cCount = this.clusterCount;
+
+  this.clusters[cCount] = new Cluster(cCount);
+  this.clusterCount++;
+
+  var cluster = this.clusters[cCount];
+  cluster.addNode(nodeValue, this.totalNodeCount);
+  this.totalNodeCount++;
+
 };
 
-Graph.prototype.hasEdge = function(fromNode, toNode){
+Cluster.prototype.addNode = function(nodeValue, id){
+  var nCount = this.nodeCount;
+
+  this.nodes[nCount] = new gNode(id);
+  this.nodeCount++;
+  this.nodes[nCount].value = nodeValue;
+};
+
+Graph.prototype.contains = function(nodeValue){
+  for(var cluster in this.clusters)
+  {
+    if( this.clusters[cluster].contains(nodeValue) === true)
+    {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+Graph.prototype.indexOf = function(nodeValue){
+  var result = -1;
+  for(var cluster in this.clusters)
+  {
+    result = this.clusters[cluster].indexOf(nodeValue);
+    if(result !== -1)
+    {
+      return result;
+    }
+  }
+  return result;
+}
+
+Graph.prototype.indexOfExperiment = function(nodeValue){
+  var result = -1;
+  for(var cluster in this.clusters)
+  {
+    result = this.clusters[cluster].indexOf(nodeValue);
+    if(result !== -1)
+    {
+      return {cID : cluster, nID : result};
+    }
+  }
+  return {cID : -1, nID : -1};;
+}
+
+Cluster.prototype.indexOf = function(nodeValue){
+  for(var node in this.nodes)
+  {
+    if(this.nodes[node].value === nodeValue)
+    {
+      return this.nodes[node].id;
+    }
+  }
+
+  return -1;
+};
+
+Cluster.prototype.contains = function(nodeValue){
+  return (this.indexOf(nodeValue) !== -1);
+};
+
+Graph.prototype.removeNode = function(nodeValue){
+  for(var cluster in this.clusters)
+  {
+    if( this.clusters[cluster].contains(nodeValue) === true)
+    {
+      this.clusters[cluster].removeNode(nodeValue);
+    }
+  }
+};
+
+//we can optimize this
+Cluster.prototype.removeNode = function(nodeValue){
+  var nIndex = this.indexOf(nodeValue);
+
+  delete this.nodes[nIndex];
+  this.nodeCount--;
+
+  return this.nodeCount;
+};
+
+Graph.prototype.hasEdge = function(fromNodeVal, toNodeVal){
+  var fromIndex = this.indexOf(fromNodeVal);
+  var toIndex = this.indexOf(toNodeVal);
+
+  if(fromIndex !== -1 && toIndex !== -1){
+    node.adjCount++
+    node.adjList[othernode.id]
+  }
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,28 +1,50 @@
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
+  newTree.childCount = 0;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = {};
+  extend(newTree, treeMethods);
 
   return newTree;
 };
 
-
-
-
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children[this.childCount] = new Tree(value);
+  this.childCount++;
 };
 
 treeMethods.contains = function(target){
 
+  var found = false;
+  var recursiveContains = function(tree, target){
+    if(found === true)
+      return;
+
+    if(tree.value === target){
+      found = true;
+    }
+    for(var child in tree.children){
+      recursiveContains(tree.children[child], target);
+    }
+  };
+
+  recursiveContains(this, target);
+  return found;
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+//.addchild() = O(1)
+//.contains() = O(n)
